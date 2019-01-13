a95bae8c9f1a55d309f40945fdd6f6c46e1cb70a
Iryna Ivakha <cs120879div1@gmail.com>
2019-01-13 09:18:35 +0200

    updated to webpack v.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 let path = require('path');
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './public/src/js/todo/index.js',
+    entry: ['./public/src/js/todo/index.js', './public/src/css/task.css'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'jshint-loader'
-        }]
-    }
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+				},
+				exclude: /(node_modules)/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader"
+				]
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'dist/'
+						}
+					}
+				]
+			},
+		],
+	},
+	
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: './task.css',
+			chunkFilename: '[id].css'
+		})
+	],
+        
 };
